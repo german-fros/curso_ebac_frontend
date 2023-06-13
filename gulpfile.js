81e8e7212d4e711bb9f80052ac96a44fcce6e7d0
German Fros <german.fros551@gmail.com>
2023-06-13 14:07:05 -0300

    push 1

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+
+
+//COMPILADOR SASS//
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'))
+}
+
+exports.sass = compilaSass
+
+//COMPRIME JS//
+function comprimeJs() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(ulgify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+exports.javascript = comprimeJs
+
+//COMPRIME IMG//
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
+
+exports.images = comprimeImagens
+
+//FUNÇÃO DEFAULT//
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJs))
+    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens))
+}
---
+++ source/scripts/script.js
@@ -0,0 +1,4 @@
+function funcaoTeste(callback) {
+    console.log('Gulp funcionando!')
+    callback()
+}
